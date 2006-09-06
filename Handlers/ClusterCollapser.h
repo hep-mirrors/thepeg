e347b8f9ca724cd733ccf4eb0bb807a3b85ed4c3
lonnblad <none@none>
2006-09-06 20:32:59 +0000

    *** empty log message ***

--- Handlers/ClusterCollapser.h
+++ Handlers/ClusterCollapser.h
@@ -362,12 +362,6 @@ struct ClassTraits<ClusterCollapser>:
    * Return the class name.
    */
   static string className() { return "ThePEG::ClusterCollapser"; }
-  /**
-   * Return the name of the shared library to be loaded to get
-   * access to this class and every other class it uses
-   * (except the base class).
-   */
-  static string library() { return "libThePEGString.so"; }
 
 };
 
