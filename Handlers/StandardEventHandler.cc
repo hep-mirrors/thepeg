9badb039f364adc3112d61c7c5a96069ab1489c4
dgrell <none@none>
2007-09-05 15:38:15 +0000

    Temporary fix for single process runs.

--- Handlers/StandardEventHandler.cc
+++ Handlers/StandardEventHandler.cc
@@ -273,7 +273,7 @@ tStdXCombPtr StandardEventHandler::select(int bin, double weight) {
     break;
   }
   // clean up the old XComb object before switching to a new one
-  if ( theLastXComb ) theLastXComb->clean();
+  if ( xCombs().size() > 1 && theLastXComb ) theLastXComb->clean();
   theLastXComb = lastXC;
   lastXC->select(weight);
   lastXC->accept();
