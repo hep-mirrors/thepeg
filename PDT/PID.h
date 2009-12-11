5cae89e6b35db02c1a7dd803efab9ab4bb677450
dgrell <none@none>
2009-12-11 14:18:30 +0000

    Started particle ID implementation.

---
+++ PDT/PID.h
@@ -0,0 +1,74 @@
+// -*- C++ -*-
+//
+// PID.h is a part of ThePEG - Toolkit for HEP Event Generation
+// Copyright (C) 1999-2007 Leif Lonnblad
+//
+// ThePEG is licenced under version 2 of the GPL, see COPYING for details.
+// Please respect the MCnet academic guidelines, see GUIDELINES for details.
+//
+#ifndef ThePEG_PID_H
+#define ThePEG_PID_H
+
+#include "EnumParticles.h"
+
+namespace ThePEG {
+
+/**
+ * PID is a helper class implementing the type of PDG particle ids. At
+ * the moment it just converts to a long, and other conversions are
+ * intercepted.
+ *
+ * @see ParticleData
+ */
+class PID {
+public:
+  /**
+   * Generic construction is disallowed.
+   */
+  template <typename T>
+  PID(T t) { 
+    t.ERROR_only_use_long_as_Particle_ID_type(); 
+  }
+
+  /**
+   * Casting to generic types is disallowed.
+   */
+  template <typename T>
+  operator T() { 
+    T t; 
+    t.ERROR_only_use_long_as_Particle_ID_type(); 
+    return t;
+  }
+
+  /**
+   * Casting to generic const types is disallowed.
+   */
+  template <typename T>
+  operator T() const { 
+    T t; 
+    t.ERROR_only_use_long_as_Particle_ID_type(); 
+    return t;
+  }
+
+private:
+  /**
+   * The particle id.
+   */
+  long id;
+};
+
+/// Specialized constructor for 'long'
+template <> inline PID::PID(long t) : id(t) {} 
+
+/// Specialized constructor for Particle Code enum
+template <> inline PID::PID(ParticleID::ParticleCodes t) : id(t) {} 
+
+/// Specialized cast for 'long'
+template <> inline PID::operator long() { return id; }
+
+/// Specialized cast for 'const long'
+template <> inline PID::operator long() const { return id; }
+
+}
+
+#endif /* ThePEG_PID_H */
