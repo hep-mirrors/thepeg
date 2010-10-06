3e654364db0a27d70b3ac5c71df0d535da2af7eb
lonnblad <none@none>
2010-10-06 21:04:56 +0000

    Added template specialization.

--- Utilities/LoopGuard.h
+++ Utilities/LoopGuard.h
@@ -78,6 +78,67 @@ private:
 
 };
 
+/**
+ * A LoopGuard object can be used to throw an exception if a loop is
+ * iterated too many times. It is used by constructing an object
+ * before the loop giving the maximum number of iterations allowed and
+ * a message to be used as argument to the constructor of the
+ * exception to be thrown. Inside the loop the parenthesis is called
+ * without argument, which will increment and check an internal
+ * counter.  This specialization is for the case where the exception
+ * class cannot be created with a message.
+ * 
+ *
+ * @see Level
+ */
+template <typename ExceptionT>
+class LoopGuard<ExceptionT,void> {
+
+public:
+
+  /**
+   * Create a loop guard object which will throw an exception of type
+   * ExceptionT, constructed with 'mess' as argument, if the maximum
+   * number of iterations is exceeded.
+   */
+  LoopGuard(long maxc = 1000000 )
+    : count(0), maxCount(maxc) {}
+
+  /**
+   * Increase the iteration count and throw an ExceptionT if the
+   * maximum number of iterations is exceeded.
+   */
+  void operator()()
+  {
+    if ( ++count > maxCount ) throw ExceptionT();
+  }
+
+private:
+
+  /**
+   * The number of counts so far.
+   */
+  long count;
+
+  /**
+   * The maximum number of counts allowed.
+   */
+  long maxCount;
+
+private:
+
+  /**
+   * Default constructor is private and not implemented.
+   */
+  LoopGuard();
+
+  /**
+   * Copy constructor is private and not implemented.
+   */
+  LoopGuard(const LoopGuard &);
+
+};
+
 }
 
 #endif /* ThePEG_LoopGuard_H */
