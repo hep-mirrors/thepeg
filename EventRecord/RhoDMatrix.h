9c1c513e89ad7339c9e72411a8764e972c6a8e0a
Peter Richardson <Peter.Richardson@durham.ac.uk>
2018-06-19 11:59:30 +0200

    add reset in rho matrix

--- EventRecord/RhoDMatrix.h
+++ EventRecord/RhoDMatrix.h
@@ -90,7 +90,19 @@ public:
 	_matrix[ix][iy]*=invnorm;
   }
   //@}
-
+  
+  /**
+   *  Reset
+   */
+  void reset(bool average = true) {
+    for(size_t ix=0; ix<_ispin; ++ix)
+      for(size_t iy=0; iy<_ispin; ++iy)
+	_matrix[ix][iy]=0.;
+    if ( average )
+      for(size_t ix=0; ix<_ispin; ++ix)
+	_matrix[ix][ix] = 1./_ispin;
+  }
+    
   /** @name Access the spin. */
   //@{
 
